22f3ec30b2deea5a7bef290db80a3b55d5917099
Ruben Ocasio <rocasio@rubenocasio.com>
2023-08-31 18:20:17 -0400

    Playground Assignment

--- Flask/playground/server.py
+++ Flask/playground/server.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-# @app.route('/')
-# def index():
-#     return render_template("index.html", phrase="hello", times=5)	# notice the 2 new named arguments!
+@app.route('/')
+def index():
+    return render_template("home.html", phrase="hello")
 
 @app.route('/play')
 def indexPlay():
