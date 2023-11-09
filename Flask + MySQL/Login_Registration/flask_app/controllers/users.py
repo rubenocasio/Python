3234cd7fc1dedcca62d7317f8320e257602fc067
Ruben Ocasio <rocasio@rubenocasio.com>
2023-11-09 15:06:08 -0500

    Login & Registration

---
+++ Flask + MySQL/Login_Registration/flask_app/controllers/users.py
@@ -0,0 +1,53 @@
+from flask import render_template, redirect, request, session, flash
+from flask_bcrypt import Bcrypt
+
+from flask_app import app
+bcrypt = Bcrypt(app)
+
+from flask_app.models.user import User
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.post('/register/user')
+def register():
+    if not User.validate(request.form):
+        return redirect('/')
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+    print(pw_hash)
+    data = {
+        "first_name": request.form['first_name'],
+        "last_name": request.form['last_name'],
+        "email": request.form['email'],
+        "password" : pw_hash
+    }
+    user_id = User.save(data)
+    session['user_id'] = user_id
+    return redirect("/dashboard")
+
+@app.post('/login')
+def login():
+    data = { "email" : request.form["email"] }
+    user_in_db = User.get_by_email(data)
+    if not user_in_db:
+        flash("Invalid Email")
+        return redirect("/")
+    if not bcrypt.check_password_hash(user_in_db.password, request.form['password']):
+        flash("Invalid Password")
+        return redirect('/')
+    session['user_id'] = user_in_db.id
+    return redirect("/dashboard")
+
+@app.route('/dashboard')
+def success():
+    if 'user_id' in session:
+        users = User.get_all()
+        return render_template('dashboard.html', users = users)
+    return redirect('/')
+
+@app.post('/logout')
+def logout_session():
+    if 'user_id' in session:
+        session.pop('user_id', None)
+    return redirect('/')
